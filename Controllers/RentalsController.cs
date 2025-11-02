43eb1818237831b5e7bf67fe1bea913d86778b37
João Neves <joao.neves@beetools.com.br>
2025-11-02 20:31:55 -0300

    rentals wip

--- Controllers/RentalsController.cs
+++ Controllers/RentalsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using BikeBuster.Models;
+using BikeBuster.Models.DTOs;
+using BikeBuster.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BikeBuster.Controllers
@@ -7,28 +12,72 @@ namespace BikeBuster.Controllers
     [ApiController]
     public class RentalsController : ControllerBase
     {
+
+
+        private readonly RentalService _rentalService;
+
+        public RentalsController(RentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
         [HttpPost]
-        public IActionResult Rent()
+        public async Task<IActionResult> Rent([FromBody][Required] RentalModel rent)
         {
-            return Ok(new { Message = "rented sucessfully." });
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                var result = await _rentalService.Create(rent);
+                return Created("", result);
+            }
+
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { Erro = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Erro = ex.Message });
+            }
+
         }
-
-
         [HttpGet("{id}")]
-        public IActionResult GetRental()
+        public async Task<IActionResult> GetById([Required] string id)
         {
-            return Ok(new { Message = "rental info accessed successfully." });
+            var bike = await _rentalService.GetByIdAsync(id);
+            if (bike == null)
+                return NotFound(); // 404 - esse ID específico não existe
+
+            return Ok(bike); // 200 - encontrou a moto
         }
 
-       
+
         [HttpPut("{id}/devolucao")]
-        public IActionResult Return()
+        public async Task<IActionResult> ReturnAsync([Required] string id, [FromBody][Required] RentalReturnRequest body)
         {
-            return Ok(new { Message = "bike was returned" });
+            try
+            {
+                var (rental, total) = await _rentalService.ReturnAsync(id, body.ReturnDate);
+
+                return Ok(new
+                {
+                    mensagem = "Data de devolução informada com sucesso",
+                    entregador_id = rental.UserId,
+                    moto_id = rental.BikeId,
+                    data_inicio = rental.ContractStartDate.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                    data_devolucao = rental.ContractEndDate?.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                    data_prevista = rental.ContractExpectedEndDate.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                    valor_total = total,
+                    aluguel_id = rental.Id,
+                });
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound(new { mensagem = "Locação não encontrada" });
+            }
+
         }
-
-
-
-
     }
 }
