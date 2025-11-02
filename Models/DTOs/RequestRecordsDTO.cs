6141189a7dd6e3a7c4389a500117208f3969fe37
João Neves <joao.neves@beetools.com.br>
2025-11-02 20:32:44 -0300

    fix json name

--- Models/DTOs/RequestRecordsDTO.cs
+++ Models/DTOs/RequestRecordsDTO.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace BikeBuster.Models.DTOs;
 
 public record LicenseUploadRequest([property: JsonPropertyName("imagem_cnh")] string ImageBase64);
 public record RentalReturnRequest([property: JsonPropertyName("data_devolucao")] DateTime ReturnDate);
-public record PlateUpdateRequest([property: JsonPropertyName("nova_placa")] string NewPlate);
+public record PlateUpdateRequest([property: JsonPropertyName("placa")] string NewPlate);
+
