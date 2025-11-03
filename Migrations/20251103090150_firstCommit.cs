a3506756f11b63e6f0460b11e00f5ef0d9c48b91
João Neves <joao.neves@beetools.com.br>
2025-11-03 06:02:09 -0300

    updated migrations

---
+++ Migrations/20251103090150_firstCommit.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BikeBuster.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstCommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bike",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Plate = table.Column<string>(type: "character varying(7)", maxLength: 7, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bike", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notification",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BikeId = table.Column<string>(type: "text", nullable: false),
+                    ReceivedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notification", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rent",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Plan = table.Column<int>(type: "integer", nullable: false),
+                    DailyRate = table.Column<decimal>(type: "numeric", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    BikeId = table.Column<string>(type: "text", nullable: false),
+                    ContractStartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ContractEndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ContractExpectedEndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rent", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DriverLicenseType = table.Column<string>(type: "text", nullable: false),
+                    DriverLicenseImage = table.Column<string>(type: "text", nullable: false),
+                    DriverLicenseNumber = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    Cnpj = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bike_Plate",
+                table: "Bike",
+                column: "Plate",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Cnpj",
+                table: "User",
+                column: "Cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_DriverLicenseNumber",
+                table: "User",
+                column: "DriverLicenseNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bike");
+
+            migrationBuilder.DropTable(
+                name: "Notification");
+
+            migrationBuilder.DropTable(
+                name: "Rent");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
